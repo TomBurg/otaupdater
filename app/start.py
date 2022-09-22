b40d37ea0e08012b53dd5e00fd0baf8ee66773b0
TomBee <13107614+TomBurg@users.noreply.github.com>
2022-09-22 16:10:13 -0400

    Update start.py

--- app/start.py
+++ app/start.py
@@ -1,1 +1,10 @@
-print('Version 2 installed using USB') 
+from rtns import SUBRTNSBW
+
+sub = SUBRTNSBW(50)
+
+sub.blink_cnt(5)
+print('Version 2 XXX installed using USB') 
+
+print('Version YYY installed ') 
+
+print('Version ZZZ installed ') 
